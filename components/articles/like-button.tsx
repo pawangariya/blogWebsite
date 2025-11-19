a60a716c3320d60ea6ca0a4b43b61f79b874c6de
pawangariya <140097175+pawangariya@users.noreply.github.com>
2025-11-19 12:31:07 +0530

    fINAL FIX: Replaced remaining Prisma type import in like-button.tsx

--- components/articles/like-button.tsx
+++ components/articles/like-button.tsx
@@ -3,7 +3,7 @@ import { Bookmark, Share2, ThumbsUp } from 'lucide-react'
 import React, { useOptimistic, useTransition } from 'react'
 import { Button } from '../ui/button'
 import { likeDislikeToggle } from '@/actions/like-dislike'
-import { Like } from '@/app/generated/prisma'
+import { Like } from '@prisma/client'
 
 type LikeButtonProps = {
     articleId:string;
